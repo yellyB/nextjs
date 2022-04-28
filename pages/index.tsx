c404bb302689f9114a81fd170d518ab45f1b3244
bom\bomi <kk3p1q1@gmail.com>
2022-04-28 21:20:57 +0900

    스타일드 컴포넌트 설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { NextPage } from "next";
 
-export default function Home() {
-  return <div className={styles.container}>hello</div>;
-}
+const index: NextPage = () => {
+  return <div>hellowfdfsdfsdf</div>;
+};
+
+export default index;
+
+/*
+index뒤에 콜론(:)은 해당 변수에 타입을 지정해주는것
+컴포넌트의 타입은 NextPage
+*/
