f56b8fc6ecc7206b1d3e268e6a84aeb62c8a9b00
bom\bomi <kk3p1q1@gmail.com>
2022-04-28 21:56:54 +0900

    본격 프로젝트 세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,12 @@ import { NextPage } from "next";
 import styled from "styled-components";
 
 const Container = styled.div`
-  padding: 20px;
+  font-size: 21px;
+  color: gray;
 `;
 
-const index: NextPage = () => {
-  return <Container>hellowfdfsdfsdf</Container>;
+const index: React.FC = () => {
+  return <Container>hello world</Container>;
 };
 
 export default index;
-
-/*
-index뒤에 콜론(:)은 해당 변수에 타입을 지정해주는것
-컴포넌트의 타입은 NextPage
-*/
