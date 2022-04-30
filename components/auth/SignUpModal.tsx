c1e54d4d72031639101e4863b90cc03901b382aa
bom\bomi <kk3p1q1@gmail.com>
2022-04-30 15:24:06 +0900

    회원가입 인풋 - 공통 컴포넌트로 변경

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CloseXIcon from "../../public/static/svg/modal/close_icon.svg";
 import MailIcon from "../../public/static/svg/modal/email.svg";
@@ -6,6 +6,7 @@ import PersonIcon from "../../public/static/svg/modal/person.svg";
 import EyeOpendIcon from "../../public/static/svg/modal/eye_closed.svg";
 import EyeClosedIcon from "../../public/static/svg/modal/eye_opend.svg";
 import palette from "../../styles/palette";
+import Input from "../common/Input";
 
 const Container = styled.div`
   width: 568px;
@@ -13,59 +14,102 @@ const Container = styled.div`
   padding: 32px;
   background-color: white;
   z-index: 11;
-
   .modal-close-x-icon {
     cursor: pointer;
     display: block;
     margin: 0 0 40px auto;
   }
-
   .input-wrapper {
     position: relative;
     margin-bottom: 16px;
-    input {
-      position: relative;
-      width: 100%;
-      height: 46px;
-      padding: 0 44px 0 11px;
-      /* border: 1px solid ${palette.gray_eb}; */
-      border-radius: 4px;
-      font-size: 16px;
-      /* outline: none; */
-      ::placeholder {
-        color: ${palette.gray_76};
-      }
-    }
+  }
+  .signup-password-input-wrapper {
     svg {
-      position: absolute;
-      right: 11px;
-      top: 11px;
-      width: 25px;
-      height: 25px;
+      cursor: pointer;
     }
   }
 `;
 
 const SignUpModal: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangeId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setId(event.target.value);
+  };
+
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const onChangePasswordConfirm = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPasswordConfirm(event.target.value);
+  };
+
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  };
+
   return (
     <Container>
       Sign Up
       <CloseXIcon className="modal-close-x-icon" />
       <div className="input-wrapper">
-        <input placeholder="email" type="email" name="email" />
-        <MailIcon />
+        <Input
+          placeholder="email"
+          type="email"
+          icon={<MailIcon />}
+          name="email"
+          value={email}
+          onChange={onChangeEmail}
+        />
       </div>
       <div className="input-wrapper">
-        <input placeholder="id" />
-        <PersonIcon />
+        <Input
+          placeholder="id"
+          icon={<PersonIcon />}
+          value={id}
+          onChange={onChangeId}
+        />
       </div>
-      <div className="input-wrapper">
-        <input placeholder="password" type="password" />
-        <EyeOpendIcon />
+      <div className="input-wrapper signup-password-input-wrapper ">
+        <Input
+          placeholder="password"
+          type={hidePassword ? "password" : "input"}
+          icon={
+            hidePassword ? (
+              <EyeOpendIcon onClick={toggleHidePassword} />
+            ) : (
+              <EyeClosedIcon onClick={toggleHidePassword} />
+            )
+          }
+          value={password}
+          onChange={onChangePassword}
+        />
       </div>
-      <div className="input-wrapper">
-        <input placeholder="password confirm" type="password" />
-        <EyeOpendIcon />
+      <div className="input-wrapper signup-password-input-wrapper ">
+        <Input
+          placeholder="password confirm"
+          type={hidePassword ? "password" : "input"}
+          icon={
+            hidePassword ? (
+              <EyeOpendIcon onClick={toggleHidePassword} />
+            ) : (
+              <EyeClosedIcon onClick={toggleHidePassword} />
+            )
+          }
+          value={passwordConfirm}
+          onChange={onChangePasswordConfirm}
+        />
       </div>
     </Container>
   );
